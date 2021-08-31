eafc5b0e266ba6fd5c3fa09d619cba11532b3859
Alessio Saltarin <alessiosaltarin@gmail.com>
2021-08-31 14:46:42 +0200

    All hosts permitted

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ COPY requirements.txt .
 RUN pip install -r requirements.txt
 COPY . .
 EXPOSE 8080
+RUN chmod 664 .
 RUN chmod +x run_unicorn.sh
 ENTRYPOINT ["/bin/bash", "/app/run_unicorn.sh"]
-
--- SmartDjango/settings.py
+++ SmartDjango/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fake'
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', ]
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
